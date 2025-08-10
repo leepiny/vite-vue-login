47614573c3550e319ed37f4c4404bb814ca5157a
leepiny <57059608+leepiny@users.noreply.github.com>
2025-08-10 21:51:34 +0800

    fix: set vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vite-vue-login/',   // ← 換成你的 github repo 名稱，前後都要有斜線
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: '/vite-vue-login/',   // ← 換成你的 github repo 名稱，前後都要有斜線
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
